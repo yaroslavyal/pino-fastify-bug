c05d9fd0b995a2db2d749e505a8371f16fd77b35
itheodoro <ithalotheodoro@hotmail.com>
2021-09-13 16:43:45 -0300

    Create NestJS project with Fastify and nestjs-pino

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { bufferLogs: true }
+  );
+
+  app.useLogger(app.get(Logger));
+
+  await app.listen(5000);
+}
+bootstrap();
